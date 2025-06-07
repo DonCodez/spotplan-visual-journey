aadb5ffc9902c6a42bced61f21dbec829efd70df
DonCodez <gordonlovabletemp@gmail.com>
2025-06-08 01:57:49 +0300

    Update gradient-text.tsx

--- src/components/ui/gradient-text.tsx
+++ src/components/ui/gradient-text.tsx
@@ -1,8 +1,7 @@
-
 "use client"
+
 import React from "react";
 import { motion, MotionProps } from "motion/react";
-
 import { cn } from "@/lib/utils";
 
 interface GradientTextProps
@@ -23,20 +22,20 @@ function GradientText({
   return (
     <MotionComponent
       className={cn(
-        "relative inline-flex overflow-hidden bg-white dark:bg-black",
+        "relative inline-block text-transparent bg-clip-text",
         className,
       )}
       {...props}
     >
-      {children}
-      <span className="pointer-events-none absolute inset-0 mix-blend-lighten dark:mix-blend-darken">
-        <span className="pointer-events-none absolute -top-1/2 h-[30vw] w-[30vw] animate-[gradient-border_6s_ease-in-out_infinite,gradient-1_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-1))] mix-blend-overlay blur-[1rem]"></span>
-        <span className="pointer-events-none absolute right-0 top-0 h-[30vw] w-[30vw] animate-[gradient-border_6s_ease-in-out_infinite,gradient-2_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-2))] mix-blend-overlay blur-[1rem]"></span>
-        <span className="pointer-events-none absolute bottom-0 left-0 h-[30vw] w-[30vw] animate-[gradient-border_6s_ease-in-out_infinite,gradient-3_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-3))] mix-blend-overlay blur-[1rem]"></span>
-        <span className="pointer-events-none absolute -bottom-1/2 right-0 h-[30vw] w-[30vw] animate-[gradient-border_6s_ease-in-out_infinite,gradient-4_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-4))] mix-blend-overlay blur-[1rem]"></span>
+      <span className="relative z-10">{children}</span>
+      <span className="pointer-events-none absolute inset-0 overflow-hidden">
+        <span className="pointer-events-none absolute -top-1/4 -left-1/4 w-[150%] h-[150%] animate-[gradient-border_6s_ease-in-out_infinite,gradient-1_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-1))] mix-blend-overlay blur-[1rem]" />
+        <span className="pointer-events-none absolute -top-1/4 -right-1/4 w-[150%] h-[150%] animate-[gradient-border_6s_ease-in-out_infinite,gradient-2_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-2))] mix-blend-overlay blur-[1rem]" />
+        <span className="pointer-events-none absolute -bottom-1/4 -left-1/4 w-[150%] h-[150%] animate-[gradient-border_6s_ease-in-out_infinite,gradient-3_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-3))] mix-blend-overlay blur-[1rem]" />
+        <span className="pointer-events-none absolute -bottom-1/4 -right-1/4 w-[150%] h-[150%] animate-[gradient-border_6s_ease-in-out_infinite,gradient-4_12s_ease-in-out_infinite_alternate] bg-[hsl(var(--color-4))] mix-blend-overlay blur-[1rem]" />
       </span>
     </MotionComponent>
   );
 }
 
-export { GradientText }
+export { GradientText };
